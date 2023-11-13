d28d7c8b19994016e29924232c2e9ab35515f909
aouraghse <aouraghse@gmail.com>
2023-11-13 14:11:11 -0800

    console

--- console.py
+++ console.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Ce module active l'interface en ligne de commande Python
+This module activates python cli
 """
 import json
 import cmd
@@ -17,7 +17,7 @@ import shlex
 
 
 def ev(val):
-    """convertit les arguments appropriés en entier ou en décimal"""
+    """converts suitable arguments to int or float"""
     for i in val:
         try:
             yield json.loads(i)
@@ -29,53 +29,54 @@ def check_arg(arg2, msg):
     try:
         command, new_id = arg2.split('(')
     except Exception:
-        print("** commande invalide **")
+        print("** invalid command **")
         return None
     if command != msg:
-        print("** commande invalide **")
+        print("** invalid command **")
         return None
     return new_id.replace(')', '')
 
 
 class HBNBCommand(cmd.Cmd):
-    """Interface en ligne de commande pour le clone AirBnB"""
+    """CLI for AirBnB clone"""
     prompt = '(hbnb) '
     file = None
-    classes = ['BaseModel', 'Place', 'State', 'City', 'Amenity', 'Review', 'User']
+    classes = ['BaseModel', 'Place', 'State',
+               'City', 'Amenity', 'Review', 'User']
 
     def do_EOF(self, line):
-        """Gère la fin de fichier (Ctrl + D)"""
+        """Handles EOF(Ctrl + D)"""
         print("")
         return True
 
     def do_quit(self, arg):
-        """Commande Quitter pour quitter le programme"""
+        """Quit command to exit the program"""
         quit()
 
     def do_create(self, args):
-        """Crée une nouvelle instance d'une classe"""
+        """Creates a new instance of a class"""
         arg2 = shlex.split(args)
         if len(arg2) < 1:
-            print("** nom de classe manquant **")
+            print("** class name missing **")
             return
         if arg2[0] not in self.classes:
-            print("** la classe n'existe pas **")
+            print("** class doesn't exist **")
             return
         model = eval(arg2[0])()
         model.save()
         print(model.id)
 
     def do_show(self, args):
-        """Affiche la représentation en chaîne d'une instance"""
+        """Prints the string representation of an instance """
         arg2 = shlex.split(args)
         if len(arg2) < 1:
-            print("** nom de classe manquant **")
+            print("** class name missing **")
             return
         if arg2[0] not in self.classes:
-            print("** la classe n'existe pas **")
+            print("** class doesn't exist **")
             return
         if len(arg2) < 2:
-            print("** identifiant d'instance manquant **")
+            print("** instance id missing **")
             return
         storage = FileStorage()
         storage.reload()
@@ -85,19 +86,19 @@ class HBNBCommand(cmd.Cmd):
             obj = tempD.get(y)
             print(obj)
         else:
-            print("** aucune instance trouvée **")
+            print("** no instance found **")
 
     def do_destroy(self, args):
-        """Supprime une instance en fonction du nom de la classe et de l'identifiant"""
+        """Deletes an instance based on the class name and id"""
         arg2 = shlex.split(args)
         if len(arg2) < 1:
-            print("** nom de classe manquant **")
+            print("** class name missing **")
             return
         if arg2[0] not in self.classes:
-            print("** la classe n'existe pas **")
+            print("** class doesn't exist **")
             return
         if len(arg2) < 2:
-            print("** identifiant d'instance manquant **")
+            print("** instance id missing **")
             return
         storage = FileStorage()
         storage.reload()
@@ -107,13 +108,13 @@ class HBNBCommand(cmd.Cmd):
             del tempD[y]
             storage.save()
         else:
-            print("** aucune instance trouvée **")
+            print("** no instance found **")
 
     def do_all(self, args):
-        """Crée une nouvelle instance d'une classe"""
+        """Creates a new instance of a class"""
         arg2 = shlex.split(args)
         if len(arg2) >= 1 and arg2[0] not in self.classes:
-            print("** la classe n'existe pas **")
+            print("** class doesn't exist **")
             return
         storage = FileStorage()
         storage.reload()
@@ -129,30 +130,30 @@ class HBNBCommand(cmd.Cmd):
         print(alList)
 
     def do_update(self, args):
-        """Met à jour une instance en fonction du nom de la classe et de l'identifiant en ajoutant
-        ou en mettant à jour l'attribut"""
+        """Updates an instance based on the class name and id by adding
+        or updating attribute"""
         arg2 = shlex.split(args)
         if len(arg2) < 1:
-            print("** nom de classe manquant **")
+            print("** class name missing **")
             return
         if arg2[0] not in self.classes:
-            print("** la classe n'existe pas **")
+            print("** class doesn't exist **")
             return
         if len(arg2) < 2:
-            print("** identifiant d'instance manquant **")
+            print("** instance id missing **")
             return
         storage = FileStorage()
         storage.reload()
         tempD = storage.all()
         y = "{}.{}".format(arg2[0], arg2[1])
         if y not in tempD.keys():
-            print("** aucune instance trouvée **")
+            print("** no instance found **")
             return
         if len(arg2) < 3:
-            print("** nom d'attribut manquant **")
+            print("** attribute name missing **")
             return
         if len(arg2) < 4:
-            print("** valeur manquante **")
+            print("** value missing **")
             return
         arg2 = list(ev(arg2))
         obj = tempD.get(y)
@@ -165,10 +166,11 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
         return
 
     def default(self, args):
-        """Gère les autres commandes"""
+        """Handles other commands"""
         args = shlex.split(args)
         try:
             arg1, arg2 = args[0].split('.')
@@ -176,10 +178,10 @@ class HBNBCommand(cmd.Cmd):
                 for i in range(1, len(args)):
                     arg2 += args[i]
         except Exception:
-            print("** commande invalide **")
+            print("** invalid command **")
             return
         if arg1 not in self.classes:
-            print("** la classe n'existe pas ")
+            print("** class doesn't exist ")
             return
         storage = FileStorage()
         storage.reload()
@@ -191,22 +193,22 @@ class HBNBCommand(cmd.Cmd):
 
         if arg2 == "all()":
             """
-            Récupère toutes les instances
-            Utilisation: <nom de la classe>.all()
+            Retrives all instances
+            Usage: <class name>.all()
             """
             print(new_list)
 
         elif arg2 == "count()":
             """
-            Récupère le nombre d'instances de la classe
-            Utilisation: <nom de la classe>.count()
+            Retrieves number of instances of class
+            Usage: <class name>.count()
             """
             print(len(new_list))
 
         elif "show" in arg2:
             """
-            Récupère une instance en fonction de l'id
-            Utilisation: <nom de la classe>.show(<id>)
+            Retrieves instance based on id
+            Usage: <class name>.show(<id>)
             """
             new_id = check_arg(arg2, "show")
             if not new_id:
@@ -215,12 +217,12 @@ class HBNBCommand(cmd.Cmd):
                 if new_id in items:
                     print(items)
                     return
-            print("** aucune instance trouvée **")
+            print("** no instance found **")
 
         elif "destroy" in arg2:
             """
-            Détruit une instance en fonction de son id
-            Utilisation: <nom de la classe>.destroy(<id>)
+            Destroys instance based on its id
+            Usage: <class name>.destroy(<id>)
             """
             new_id = check_arg(arg2, "destroy")
             if not new_id:
@@ -230,12 +232,12 @@ class HBNBCommand(cmd.Cmd):
                 del tempD[y]
                 storage.save()
             else:
-                print("** aucune instance trouvée **")
+                print("** no instance found **")
 
         elif "update" in arg2:
             """
-            Met à jour une instance en fonction de l'id
-            Utilisation: <nom de la classe>.update(<id>, <nom>, <valeur>)
+            Updates instance based on id
+            Usage: <class name>.update(<id>, <name>, <value>)
             """
             new_arg = check_arg(arg2, "update")
             if not new_arg:
@@ -244,7 +246,7 @@ class HBNBCommand(cmd.Cmd):
                 try:
                     new_list = list(new_arg.split('{'))
                 except Exception:
-                    print("** arguments invalides **")
+                    print("** invalid arguments **")
                     return
                 new_id = new_list[0].replace(',', '')
                 new_dict = new_list[1].replace('}', '')
@@ -257,14 +259,14 @@ class HBNBCommand(cmd.Cmd):
                             valx = list(item.split(':'))
                             valx = list(ev(valx))
                         except Exception:
-                            print("** nom d'attribut manquant **")
+                            print("** attribute name missing **")
                             return
                         setattr(tempD[y], valx[0], valx[1])
                     tempD[y].save()
                     storage.save()
 
                 else:
-                    print("** aucune instance trouvée **")
+                    print("** no instance found **")
 
             elif len(args) == 3:
                 new_id, new_key, new_value = new_arg.split(',')
@@ -277,11 +279,10 @@ class HBNBCommand(cmd.Cmd):
                     setattr(tempD[y], new_key, new_value)
                     storage.save()
                 else:
-                    print("** aucune instance trouvée **")
+                    print("** no instance found **")
             else:
-                print("** attribut manquant **")
+                print("** attribute missing **")
 
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
