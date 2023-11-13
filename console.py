96ef9a132e8be89dd86c84e561a41bcc75bc33f8
aouraghse <aouraghse@gmail.com>
2023-11-13 14:09:55 -0800

    console

--- console.py
+++ console.py
@@ -5,35 +5,43 @@ Ce module active l'interface en ligne de commande Python
 import json
 import cmd
 import sys
-import shlex
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.city import City
 from models.engine.file_storage import FileStorage
+import shlex
 
-# Use a dictionary to map class names to class objects
-CLASSES = {
-    'BaseModel': BaseModel,
-    'Place': Place,
-    'State': State,
-    'City': City,
-    'Amenity': Amenity,
-    'Review': Review,
-    'User': User,
-}
 
-def load_instance(arg2):
-    """Load instance from storage based on class name and id"""
-    storage = FileStorage()
-    storage.reload()
-    tempD = storage.all()
-    y = f"{arg2[0]}.{arg2[1]}"
-    return tempD.get(y)
+def ev(val):
+    """convertit les arguments appropriés en entier ou en décimal"""
+    for i in val:
+        try:
+            yield json.loads(i)
+        except Exception:
+            yield i
 
-def print_error(message):
-    """Print error message"""
-    print(f"** {message} **")
+
+def check_arg(arg2, msg):
+    try:
+        command, new_id = arg2.split('(')
+    except Exception:
+        print("** commande invalide **")
+        return None
+    if command != msg:
+        print("** commande invalide **")
+        return None
+    return new_id.replace(')', '')
+
 
 class HBNBCommand(cmd.Cmd):
     """Interface en ligne de commande pour le clone AirBnB"""
     prompt = '(hbnb) '
+    file = None
+    classes = ['BaseModel', 'Place', 'State', 'City', 'Amenity', 'Review', 'User']
 
     def do_EOF(self, line):
         """Gère la fin de fichier (Ctrl + D)"""
@@ -47,85 +55,112 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, args):
         """Crée une nouvelle instance d'une classe"""
         arg2 = shlex.split(args)
-        if not arg2:
-            print_error("nom de classe manquant")
+        if len(arg2) < 1:
+            print("** nom de classe manquant **")
             return
-        class_name = arg2[0]
-        if class_name not in CLASSES:
-            print_error("la classe n'existe pas")
+        if arg2[0] not in self.classes:
+            print("** la classe n'existe pas **")
             return
-
-        model = CLASSES[class_name]()
+        model = eval(arg2[0])()
         model.save()
         print(model.id)
 
     def do_show(self, args):
         """Affiche la représentation en chaîne d'une instance"""
         arg2 = shlex.split(args)
+        if len(arg2) < 1:
+            print("** nom de classe manquant **")
+            return
+        if arg2[0] not in self.classes:
+            print("** la classe n'existe pas **")
+            return
         if len(arg2) < 2:
-            print_error("nom de classe ou identifiant d'instance manquant")
+            print("** identifiant d'instance manquant **")
             return
-        class_name, instance_id = arg2[0], arg2[1]
-        if class_name not in CLASSES:
-            print_error("la classe n'existe pas")
-            return
-
-        instance = load_instance(arg2)
-        if instance:
-            print(instance)
+        storage = FileStorage()
+        storage.reload()
+        tempD = storage.all()
+        y = "{}.{}".format(arg2[0], arg2[1])
+        if y in tempD.keys():
+            obj = tempD.get(y)
+            print(obj)
         else:
-            print_error("aucune instance trouvée")
+            print("** aucune instance trouvée **")
 
     def do_destroy(self, args):
         """Supprime une instance en fonction du nom de la classe et de l'identifiant"""
         arg2 = shlex.split(args)
+        if len(arg2) < 1:
+            print("** nom de classe manquant **")
+            return
+        if arg2[0] not in self.classes:
+            print("** la classe n'existe pas **")
+            return
         if len(arg2) < 2:
-            print_error("nom de classe ou identifiant d'instance manquant")
+            print("** identifiant d'instance manquant **")
             return
-        class_name, instance_id = arg2[0], arg2[1]
-        if class_name not in CLASSES:
-            print_error("la classe n'existe pas")
-            return
-
-        instance = load_instance(arg2)
-        if instance:
-            del storage.all()[f"{class_name}.{instance_id}"]
+        storage = FileStorage()
+        storage.reload()
+        tempD = storage.all()
+        y = "{}.{}".format(arg2[0], arg2[1])
+        if y in tempD.keys():
+            del tempD[y]
             storage.save()
         else:
-            print_error("aucune instance trouvée")
+            print("** aucune instance trouvée **")
 
     def do_all(self, args):
         """Crée une nouvelle instance d'une classe"""
         arg2 = shlex.split(args)
-        if arg2 and arg2[0] not in CLASSES:
-            print_error("la classe n'existe pas")
+        if len(arg2) >= 1 and arg2[0] not in self.classes:
+            print("** la classe n'existe pas **")
             return
-
         storage = FileStorage()
         storage.reload()
         tempD = storage.all()
-        instances = [str(obj) for obj in tempD.values() if not arg2 or arg2[0] in str(obj)]
-        print(instances)
+        alList = []
+
+        for key, obj in tempD.items():
+            if len(arg2) >= 1:
+                if args[0] in key:
+                    alList.append(str(obj))
+            else:
+                alList.append(str(obj))
+        print(alList)
 
     def do_update(self, args):
         """Met à jour une instance en fonction du nom de la classe et de l'identifiant en ajoutant
         ou en mettant à jour l'attribut"""
         arg2 = shlex.split(args)
+        if len(arg2) < 1:
+            print("** nom de classe manquant **")
+            return
+        if arg2[0] not in self.classes:
+            print("** la classe n'existe pas **")
+            return
+        if len(arg2) < 2:
+            print("** identifiant d'instance manquant **")
+            return
+        storage = FileStorage()
+        storage.reload()
+        tempD = storage.all()
+        y = "{}.{}".format(arg2[0], arg2[1])
+        if y not in tempD.keys():
+            print("** aucune instance trouvée **")
+            return
+        if len(arg2) < 3:
+            print("** nom d'attribut manquant **")
+            return
         if len(arg2) < 4:
-            print_error("arguments insuffisants")
+            print("** valeur manquante **")
             return
-        class_name, instance_id, attribute_name, value = arg2[0], arg2[1], arg2[2], arg2[3]
-        if class_name not in CLASSES:
-            print_error("la classe n'existe pas")
-            return
-
-        instance = load_instance(arg2)
-        if not instance:
-            print_error("aucune instance trouvée")
-            return
-
-        setattr(instance, attribute_name, json.loads(value))
-        instance.save()
+        arg2 = list(ev(arg2))
+        obj = tempD.get(y)
+        dict2 = obj.to_dict()
+        dict2.update({arg2[2]: arg2[3]})
+        obj2 = eval(arg2[0])(**dict2)
+        obj2.save()
+        tempD.update({y: obj2})
         storage.save()
 
     def emptyline(self):
@@ -136,82 +171,116 @@ class HBNBCommand(cmd.Cmd):
         """Gère les autres commandes"""
         args = shlex.split(args)
         try:
-            class_name, command = args[0].split('.')
+            arg1, arg2 = args[0].split('.')
             if len(args) > 1:
-                command += ''.join(args[1:])
+                for i in range(1, len(args)):
+                    arg2 += args[i]
         except Exception:
-            print_error("commande invalide")
+            print("** commande invalide **")
             return
-
-        if class_name not in CLASSES:
-            print_error("la classe n'existe pas")
+        if arg1 not in self.classes:
+            print("** la classe n'existe pas ")
             return
-
         storage = FileStorage()
         storage.reload()
         tempD = storage.all()
-        new_list = [str(value) for key, value in tempD.items() if class_name in key]
+        new_list = []
+        for key, value in tempD.items():
+            if arg1 in key:
+                new_list.append(str(value))
 
-        if command == "all()":
+        if arg2 == "all()":
+            """
+            Récupère toutes les instances
+            Utilisation: <nom de la classe>.all()
+            """
             print(new_list)
-        elif command == "count()":
+
+        elif arg2 == "count()":
+            """
+            Récupère le nombre d'instances de la classe
+            Utilisation: <nom de la classe>.count()
+            """
             print(len(new_list))
-        elif "show" in command:
-            new_id = check_arg(command, "show")
+
+        elif "show" in arg2:
+            """
+            Récupère une instance en fonction de l'id
+            Utilisation: <nom de la classe>.show(<id>)
+            """
+            new_id = check_arg(arg2, "show")
             if not new_id:
                 return
             for items in new_list:
                 if new_id in items:
                     print(items)
                     return
-            print_error("aucune instance trouvée")
-        elif "destroy" in command:
-            new_id = check_arg(command, "destroy")
+            print("** aucune instance trouvée **")
+
+        elif "destroy" in arg2:
+            """
+            Détruit une instance en fonction de son id
+            Utilisation: <nom de la classe>.destroy(<id>)
+            """
+            new_id = check_arg(arg2, "destroy")
             if not new_id:
                 return
-            y = f"{class_name}.{new_id}"
+            y = f"{arg1}.{new_id}"
             if y in tempD.keys():
                 del tempD[y]
                 storage.save()
             else:
-                print_error("aucune instance trouvée")
-        elif "update" in command:
-            new_arg = check_arg(command, "update")
+                print("** aucune instance trouvée **")
+
+        elif "update" in arg2:
+            """
+            Met à jour une instance en fonction de l'id
+            Utilisation: <nom de la classe>.update(<id>, <nom>, <valeur>)
+            """
+            new_arg = check_arg(arg2, "update")
             if not new_arg:
                 return
             if '{' in new_arg:
                 try:
                     new_list = list(new_arg.split('{'))
                 except Exception:
-                    print_error("arguments invalides")
+                    print("** arguments invalides **")
                     return
                 new_id = new_list[0].replace(',', '')
                 new_dict = new_list[1].replace('}', '')
                 new_list = list(new_dict.split(','))
-                y = f"{class_name}.{new_id}"
+                y = f"{arg1}.{new_id}"
 
                 if y in tempD.keys():
                     for item in new_list:
                         try:
-                            attribute, val = item.split(':')
-                            setattr(tempD[y], attribute, json.loads(val))
+                            valx = list(item.split(':'))
+                            valx = list(ev(valx))
                         except Exception:
-                            print_error("nom d'attribut manquant")
+                            print("** nom d'attribut manquant **")
                             return
+                        setattr(tempD[y], valx[0], valx[1])
                     tempD[y].save()
                     storage.save()
+
                 else:
-                    print_error("aucune instance trouvée")
+                    print("** aucune instance trouvée **")
+
             elif len(args) == 3:
                 new_id, new_key, new_value = new_arg.split(',')
-                y = f"{class_name}.{new_id}"
+                new_list = []
+                new_list.append(new_value)
+                new_list = list(ev(new_list))
+                new_value = new_list[0]
+                y = f"{arg1}.{new_id}"
                 if y in tempD.keys():
-                    setattr(tempD[y], new_key, json.loads(new_value))
+                    setattr(tempD[y], new_key, new_value)
                     storage.save()
                 else:
-                    print_error("aucune instance trouvée")
+                    print("** aucune instance trouvée **")
             else:
-                print_error("attribut manquant")
+                print("** attribut manquant **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
